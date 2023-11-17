859b1f19ad71dd50f85cd0187e2b41a4ee6df76e
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-17 01:56:33 -0300

    jwt

--- src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
+++ src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
@@ -4,8 +4,12 @@
  */
 package com.bytebuilders.VotoLyze.config;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.bytebuilders.VotoLyze.entidades.Eleitor;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +22,22 @@ import org.springframework.stereotype.Service;
 public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
+    
+    
     public String generateToken(Eleitor eleitor) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256();
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create().withIssuer("votolyze").
+                    withSubject(eleitor.getEmail()).withExpiresAt(1000).sign(algorithm);
             
         } catch() {
             
         }
         
+    }
+    
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
     
 }

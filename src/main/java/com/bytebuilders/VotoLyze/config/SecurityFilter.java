07716be10dafad79d297ad37451712550f1d3247
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-17 02:11:00 -0300

    auth

--- src/main/java/com/bytebuilders/VotoLyze/config/SecurityFilter.java
+++ src/main/java/com/bytebuilders/VotoLyze/config/SecurityFilter.java
@@ -1,10 +1,15 @@
 package com.bytebuilders.VotoLyze.config;
 
+import com.bytebuilders.VotoLyze.repositorios.EleitoresRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,12 +20,22 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenService tokenService;
+    
+    @Autowired
+    EleitoresRepository eleitoresRepository;
+    
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
        var token = this.recoverToken(request);
        if (token != null) {
+           var login = tokenService.validateToken(token);
+           UserDetails user = eleitoresRepository.findByEmail(login);
            
+           var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+           SecurityContextHolder.getContext().setAuthentication(authentication);
        }
        
        filterChain.doFilter(request, response);

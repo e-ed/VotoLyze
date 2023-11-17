456bfa915551294ed6669234fae5bd56f2d34453
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-16 21:42:29 -0300

    Initial commit

---
+++ src/main/java/com/bytebuilders/VotoLyze/VotoLyzeApplication.java
@@ -0,0 +1,13 @@
+package com.bytebuilders.VotoLyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotoLyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotoLyzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebuilders/VotoLyze/VotoLyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebuilders.VotoLyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoLyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

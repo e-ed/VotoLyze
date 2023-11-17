c09899149a5ca12c6bd882009c00f4347f1c2f1b
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-17 02:05:18 -0300

    jwt

---
+++ src/main/java/com/bytebuilders/VotoLyze/config/SecurityFilter.java
@@ -0,0 +1,35 @@
+package com.bytebuilders.VotoLyze.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * @author eduardo
+ */
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       var token = this.recoverToken(request);
+       if (token != null) {
+           
+       }
+       
+       filterChain.doFilter(request, response);
+    }
+    
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+    
+}
--- src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
+++ src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
@@ -7,6 +7,7 @@ package com.bytebuilders.VotoLyze.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.bytebuilders.VotoLyze.entidades.Eleitor;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -18,13 +19,12 @@ import org.springframework.stereotype.Service;
  *
  * @author eduardo
  */
-
 @Service
 public class TokenService {
+
     @Value("${api.security.token.secret}")
     private String secret;
-    
-    
+
     public String generateToken(Eleitor eleitor) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
@@ -33,17 +33,27 @@ public class TokenService {
                     withSubject(eleitor.getEmail())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
-            
+
             return token;
-            
-        } catch(JWTCreationException exc) {
-            throw new RuntimeException("JWT creation error - " + exc);
+
+        } catch (JWTCreationException exc) {
+            throw new RuntimeException("JWT generation error - " + exc);
         }
-        
+
     }
-    
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer("votolyze").build().verify(token).getSubject();
+        } catch (JWTVerificationException exc) {
+            return "";
+
+        }
+    }
+
     private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
-    
+
 }

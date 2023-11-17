f32b94f277beb57d0fc3a623f8c4e01b9c45a2f1
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-16 22:16:42 -0300

    sec

--- src/main/java/com/bytebuilders/VotoLyze/VotoLyzeApplication.java
+++ src/main/java/com/bytebuilders/VotoLyze/VotoLyzeApplication.java
@@ -2,6 +2,8 @@ package com.bytebuilders.VotoLyze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VotoLyzeApplication {
@@ -10,4 +12,18 @@ public class VotoLyzeApplication {
 		SpringApplication.run(VotoLyzeApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class HttpController {
+	@GetMapping("/public")
+	String publicRoute() {
+		return "<h1> Public route! </h1>";
+	}
+
+	@GetMapping("/private")
+	String privateRoute() {
+		return "<h1> Private route! </h1>";
+	}
 }

f8b80c4f6cc99bb42d0f7a5b74bc1aee35dfd6a0
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-17 01:57:33 -0300

    jwt

--- src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
+++ src/main/java/com/bytebuilders/VotoLyze/config/TokenService.java
@@ -6,6 +6,7 @@ package com.bytebuilders.VotoLyze.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.bytebuilders.VotoLyze.entidades.Eleitor;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -27,11 +28,16 @@ public class TokenService {
     public String generateToken(Eleitor eleitor) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create().withIssuer("votolyze").
-                    withSubject(eleitor.getEmail()).withExpiresAt(1000).sign(algorithm);
+            String token = JWT.create()
+                    .withIssuer("votolyze").
+                    withSubject(eleitor.getEmail())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
             
-        } catch() {
+            return token;
             
+        } catch(JWTCreationException exc) {
+            throw new RuntimeException("JWT creation error - " + exc);
         }
         
     }

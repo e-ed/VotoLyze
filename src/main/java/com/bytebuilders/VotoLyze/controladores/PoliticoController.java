9b57b97a3bc3c521325f96f010bed1ffe5e31828
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-11-19 23:07:15 -0300

    Politico controller

--- src/main/java/com/bytebuilders/VotoLyze/controladores/PoliticoController.java
+++ src/main/java/com/bytebuilders/VotoLyze/controladores/PoliticoController.java
@@ -1,19 +1,22 @@
 package com.bytebuilders.VotoLyze.controladores;
 
 
-import com.bytebuilders.VotoLyze.entidades.Partido;
-import com.bytebuilders.VotoLyze.entidades.Politico;
+import com.bytebuilders.VotoLyze.config.TokenService;
+import com.bytebuilders.VotoLyze.entidades.*;
 import com.bytebuilders.VotoLyze.servicos.PartidoServices;
 import com.bytebuilders.VotoLyze.servicos.PoliticoServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/politico")
@@ -22,8 +25,121 @@ public class PoliticoController {
     @Autowired
     PoliticoServices politicoServices;
 
+    @Autowired
+    TokenService tokenService;
+
+
     @GetMapping("")
     public ResponseEntity<List<Politico>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(politicoServices.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id,
+                                         @RequestBody @Valid PoliticoRegisterDTO registerDTO,
+                                         @RequestHeader("Authorization") String authorization) {
+
+        String extractedTokenFromHeader = tokenService.extractToken(authorization);
+        String userRequesting = tokenService.getUserIdFromToken(extractedTokenFromHeader);
+        Politico idFromUserRequesting = politicoServices.findByEmailIgnoreCase(userRequesting);
+
+        if (!Objects.equals(idFromUserRequesting.getId(), id)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("ID mismatch for operation");
+        }
+
+
+        Optional<Politico> toBeUpdated = politicoServices.findById(id);
+        if (toBeUpdated.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        var updatedUser = new Politico();
+
+        updatedUser.setEmail(registerDTO.login());
+        updatedUser.setSenha(toBeUpdated.get().getSenha());
+        updatedUser.setNome(registerDTO.nome());
+        updatedUser.setSexo(registerDTO.sexo().charAt(0));
+        updatedUser.setCPF(registerDTO.CPF());
+        updatedUser.setDataNascimento(Date.valueOf(registerDTO.dataNascimento().toLocalDate().plusDays(1)));
+        updatedUser.setId(toBeUpdated.get().getId());
+        updatedUser.setInicioMandato(registerDTO.inicioMandato());
+        updatedUser.setPartido(registerDTO.partido());
+        updatedUser.setTipoCandidatura(registerDTO.tipoCandidatura().getValue());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(politicoServices.save(updatedUser));
+
+    }
+
+
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Integer id) {
+        Optional<Politico> politico = politicoServices.findById(id);
+        if (!politico.isPresent()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        return ResponseEntity.status(HttpStatus.OK).body(politico);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Object> findById(@PathVariable String email) {
+        Optional<UserDetails> politico = politicoServices.findByEmail(email);
+        if (!politico.isPresent()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        return ResponseEntity.status(HttpStatus.OK).body(politico);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Integer id, @RequestHeader("Authorization") String authorization) {
+
+        String extractedTokenFromHeader = tokenService.extractToken(authorization);
+        String userRequesting = tokenService.getUserIdFromToken(extractedTokenFromHeader);
+        System.out.println(userRequesting);
+        Politico idFromUserRequesting = politicoServices.findByEmailIgnoreCase(userRequesting);
+        System.out.println("user requesting: " + idFromUserRequesting.getId());
+        System.out.println("id from URL: " + id);
+
+        if (!Objects.equals(idFromUserRequesting.getId(), id)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("ID mismatch for operation");
+        }
+
+
+        Optional<Politico> toBeDeleted = politicoServices.findById(id);
+        if (!toBeDeleted.isPresent()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        politicoServices.delete(toBeDeleted.get());
+        return ResponseEntity.status(HttpStatus.OK).body("deleted");
+    }
+
+    @PutMapping("/updatePassword/{id}")
+    public ResponseEntity<Object> updatePassword(@PathVariable Integer id,
+                                                 @RequestBody @Valid PoliticoRegisterDTO registerDTO,
+                                                 @RequestHeader("Authorization") String authorization) {
+        String extractedTokenFromHeader = tokenService.extractToken(authorization);
+        String userRequesting = tokenService.getUserIdFromToken(extractedTokenFromHeader);
+        System.out.println(userRequesting);
+        Politico idFromUserRequesting = politicoServices.findByEmailIgnoreCase(userRequesting);
+        System.out.println("user requesting: " + idFromUserRequesting.getId());
+        System.out.println("id from URL: " + id);
+
+        if (!Objects.equals(idFromUserRequesting.getId(), id)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("ID mismatch for operation");
+        }
+
+
+        Optional<Politico> toBeUpdated = politicoServices.findById(id);
+        if (toBeUpdated.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        var updatedUser = new Politico();
+        updatedUser.setEmail(toBeUpdated.get().getEmail());
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
+        updatedUser.setSenha(encryptedPassword);
+
+        updatedUser.setNome(toBeUpdated.get().getNome());
+        updatedUser.setSexo(toBeUpdated.get().getSexo());
+        updatedUser.setCPF(toBeUpdated.get().getCPF());
+        updatedUser.setDataNascimento(Date.valueOf(toBeUpdated.get().getDataNascimento().toLocalDate().plusDays(1)));
+        updatedUser.setId(toBeUpdated.get().getId());
+        updatedUser.setTipoCandidatura(toBeUpdated.get().getTipoCandidatura());
+        updatedUser.setPartido(toBeUpdated.get().getPartido());
+        updatedUser.setInicioMandato(toBeUpdated.get().getInicioMandato());
+        
+        return ResponseEntity.status(HttpStatus.OK).body(politicoServices.save(updatedUser));
+
     }
 }
--- src/main/java/com/bytebuilders/VotoLyze/servicos/PoliticoServices.java
+++ src/main/java/com/bytebuilders/VotoLyze/servicos/PoliticoServices.java
@@ -1,13 +1,17 @@
 package com.bytebuilders.VotoLyze.servicos;
 
+import com.bytebuilders.VotoLyze.entidades.Eleitor;
 import com.bytebuilders.VotoLyze.entidades.Partido;
 import com.bytebuilders.VotoLyze.entidades.Politico;
 import com.bytebuilders.VotoLyze.repositorios.PartidoRepository;
 import com.bytebuilders.VotoLyze.repositorios.PoliticoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PoliticoServices {
@@ -16,4 +20,35 @@ public class PoliticoServices {
     public List<Politico> findAll() {
         return politicoRepository.findAll();
     }
+
+    public Politico findByEmailIgnoreCase(String email) {
+        return politicoRepository.findByEmailIgnoreCase(email);
+    }
+
+    public Optional<Politico> findById(Integer id) {
+        return politicoRepository.findById(id);
+    }
+
+    @Transactional
+    public Politico save(Politico politico) {
+
+
+        return politicoRepository.save(politico);
+    }
+
+    public Optional<UserDetails> findByEmail(String email) {
+        var e = politicoRepository.findByEmail(email);
+        if (politicoRepository.findByEmail(email) != null) {
+            return Optional.ofNullable(e);
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional
+    public void delete(Politico politico) {
+        politicoRepository.delete(politico);
+    }
+
+
 }

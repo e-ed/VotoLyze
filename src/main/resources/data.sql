84f8258c4f5ec5815b3571de809fe877d4f18471
e-ed <eduardo.evangelista@testrigor.com>
2023-11-24 22:25:51 -0300

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO PARTIDOS (PRT_NOME, PRT_SIGLA) VALUES ('Partido dos Trabalhadores', 'PT');
 INSERT INTO PARTIDOS (PRT_NOME, PRT_SIGLA) VALUES ('Partido da Social Democracia Brasileira', 'PSDB');
 INSERT INTO PARTIDOS (PRT_NOME, PRT_SIGLA) VALUES ('Partido Comunista Brasileiro', 'PCB');
+INSERT INTO PARTIDOS (PRT_NOME, PRT_SIGLA) VALUES ('Partido da Causa Operária', 'PCO');
+INSERT INTO PARTIDOS (PRT_NOME, PRT_SIGLA) VALUES ('Partido Liberal', 'PL');
 
 
